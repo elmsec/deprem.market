5927e3b86cef6b7434f7def7dfe1f31255f3f986
Eyüp Can ELMA <dev@elma.dev>
2023-02-14 14:19:46 +0300

    Add API and admin urls

---
+++ deprem/urls.py
@@ -0,0 +1,48 @@
+"""
+URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from users.views import UserViewSet
+from listings.views import ListingViewSet, CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'listings', ListingViewSet)
+router.register(r'categories', CategoryViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path(
+        'api/auth/',
+        include('rest_framework.urls', namespace='rest_framework'),
+    ),
+    path(
+        'api/auth/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'),
+    path(
+        'api/auth/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'),
+    path(
+        'api/auth/token/verify/',
+        TokenVerifyView.as_view(),
+        name='token_verify'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
