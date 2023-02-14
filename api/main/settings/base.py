25fd845c27689590e8ed4902be3be7df100f2243
Eyüp Can ELMA <dev@elma.dev>
2023-02-14 16:30:28 +0300

    Move api files into the api folder

--- api/main/settings/base.py
+++ api/main/settings/base.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
