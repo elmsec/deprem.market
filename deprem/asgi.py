f6725b664d8c73994370b1d2a8b84ba28165e44b
Eyüp Can ELMA <dev@elma.dev>
2023-02-14 15:05:17 +0300

    Add settings modules for different environments

--- deprem/asgi.py
+++ deprem/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deprem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deprem.settings.base')
 
 application = get_asgi_application()
