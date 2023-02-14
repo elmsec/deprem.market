6cf93f2c3d012dc8ce2a76004c6ce3df97b02795
Eyüp Can ELMA <dev@elma.dev>
2023-02-14 14:13:33 +0300

    Add BaseModel with UUID as primary key

---
+++ deprem/models.py
@@ -0,0 +1,9 @@
+import uuid
+from django.db import models
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
